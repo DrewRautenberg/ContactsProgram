88aa227f42848a8273699b791419a742f1e0850e
Drew Rautenberg <DREW3330@gmail.com>
2017-11-02 11:49:03 -0400

    declared methods in Contacts class

--- src/contactsprogram/Contacts.java
+++ src/contactsprogram/Contacts.java
@@ -1,5 +1,43 @@
 package contactsprogram;
 
 public class Contacts {
-    
+    private static String name;
+    private static long phone;
+
+    static void setName(String pName){
+
+    }
+    static void setPhone(long pPhone){
+
+    }
+
+    static String getName(){
+        return " ";
+    }
+    static char getInitial(){
+        return 'a';
+    }
+    static long getPhone(){
+        return  269276310;
+    }
+
+    static int findAreaCode(){
+        return 269;
+    }
+    static int findLocalNumber(){
+        return 2763101;
+    }
+    static boolean is269AreaCode(){
+        return true;
+    }
+    static String nicelyFormatPhone(){
+        return "formatted phone";
+    }
+
+    static void displayData(){
+
+    }
+    //static String toString(){
+    //    return "string";
+    //}
 }
